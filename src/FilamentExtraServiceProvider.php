05224605c74c00ac2f720eb0fc4d3790d9e7c080
saade <saade@users.noreply.github.com>
2023-10-19 14:38:12 +0000

    Fix styling

--- src/FilamentExtraServiceProvider.php
+++ src/FilamentExtraServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use Saade\FilamentExtra\Commands\FilamentExtraCommand;
+use Saade\FilamentExtra\Testing\TestsFilamentExtra;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Saade\FilamentExtra\Commands\FilamentExtraCommand;
-use Saade\FilamentExtra\Testing\TestsFilamentExtra;
 
 class FilamentExtraServiceProvider extends PackageServiceProvider
 {
